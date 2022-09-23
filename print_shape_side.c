fe319f981216b17120f694dbf120704f124a82e2
Mohammad Zia <76523507+zia002@users.noreply.github.com>
2022-09-23 10:24:23 +0600

    Add files via upload

---
+++ print_shape_side.c
@@ -0,0 +1,26 @@
+/*
+Print a shape.Enter row & column value.
+this programme will print a shape side.
+*/
+#include<stdio.h>
+void main()
+{
+    int r,c,row,column;
+    printf("Enter Row & Column Number:");
+    scanf("%d %d",&row,&column);
+    for(r=1; r<=row; r++)
+    {
+        for(c=1; c<=column; c++)
+        {
+            if(r==1 || c==1 || r==row || c==column )
+            {
+                printf("★ ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
